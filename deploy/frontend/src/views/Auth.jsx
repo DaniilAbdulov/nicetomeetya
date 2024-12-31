2c038cb3298d93018b70db3eade48bcccebf61ea
Daniil Abdulov <abdulov.dany@yandex.ru>
2024-12-31 21:15:44 +0500

    ff

--- deploy/frontend/src/views/Auth.jsx
+++ deploy/frontend/src/views/Auth.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 import { observer } from "mobx-react-lite";
 import { Card, Flex } from "antd";
-import { userStore } from "../stores/UserStore.js";
-import { NavLink } from "react-router-dom";
 import "./Home.scss";
 import { AuthForm } from "#components/AuthForm/index.jsx";
 
