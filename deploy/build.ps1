6fd59f5affd37545ad74b99a6ebafefbf987538c
Daniil Abdulov <abdulov.dany@yandex.ru>
2025-01-04 19:50:32 +0500

    updates

--- deploy/build.ps1
+++ deploy/build.ps1
@@ -2,18 +2,20 @@ minikube delete
 
 minikube start
 
+cd pg
+kubectl apply -f deployment.yaml
+cd ..
+
 # Получение окружения Docker Minikube
 & minikube docker-env | Invoke-Expression
 
 # Сборка образов
-docker build -t frontend:prod .\frontend
+# docker build -t frontend:prod .\frontend
 docker build -t users:prod .\users
 docker build -t addresses:prod .\addresses
 docker build -t sympathy:prod .\sympathy
 
-cd pg
-kubectl apply -f deployment.yaml
-cd ..
+
 cd users
 kubectl apply -f deployment.yaml
 cd ..
